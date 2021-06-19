88cba6721314e3650897ecc86d79c1ec5d666c27
susanasun <85902177+susanasun@users.noreply.github.com>
2021-06-18 23:34:27 -0700

    Add files via upload

---
+++ list_practice.py
@@ -0,0 +1,25 @@
+empty_list = []
+
+city_list = ["Oakland", "Atlanta", "New York City", "Seattle", "Memphis", "Miami", "Boston", "Los Angeles", "Denver", "New Orleans"]
+print(city_list)
+print(city_list[5])
+print(city_list[0])
+print(city_list[7])
+print(city_list[2])
+
+three_cities = city_list[0:3]
+print(three_cities)
+
+US_cities = city_list[4:8]
+print(US_cities)
+city_list[0] = "San Francisco"
+city_list[2] = "Brooklyn"
+city_list[7] = "Hollywood"
+city_list[5] = "Tampa"
+print(city_list)
+
+fruit_list = ["Strawberry", "Apple", "Grape", "Mango", "Banana", "Pear", "Watermelon", "Pineapple", "Raspberry", "Blackberry", "Blueberry"]
+print(fruit_list)
+print(fruit_list[0])
+print(fruit_list[1])
+print(fruit_list[2])
