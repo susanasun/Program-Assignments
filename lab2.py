1c927239112f5b1d54a9f56c911b1b02c6310d91
susanasun <85902177+susanasun@users.noreply.github.com>
2021-06-18 23:33:14 -0700

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+name = "LeBron"
+
+age = 35
+
+salary = 37.44
+
+plays_basketball = True
+
+jersey_number = "23"
+
+car_name = "Tesla"
+x = 50
+first_name = "Susana"
+print(car_name)
+type(x)
+print(first_name)
